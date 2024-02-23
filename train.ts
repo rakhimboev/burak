013f3282a57454f25f05488c3b97e204631c4006
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2024-02-22 22:23:19 -0500

    G-task done

---
+++ train.ts
@@ -0,0 +1,13 @@
+const highest = (arr: number[]) => {
+  let max: number = 0;
+
+  for (let i: number = 1; i < arr.length; i++) {
+    if (arr[i] > arr[max]) {
+      max = i;
+    }
+  }
+  console.log(max);
+};
+
+highest([5, 21, 12, 21, 8]);
+highest([5, 5, 5, 7]);
