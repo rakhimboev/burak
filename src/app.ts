c3bb0877db10c8ca56c9e99487211b30023067b9
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2024-02-25 22:40:09 -0500

    feat: express integreyshns

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import path from "path";
+
+/* 1 - entrance */
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+
+
+/* 2 - sessions */
+
+
+
+/* 3 - views */
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+
+
+
+/* 4 - routes */
+
+export default app
