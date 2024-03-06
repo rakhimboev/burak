cfcbb18ae83b48647c5c3182d8d8e0f4549cc5c4
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2024-03-06 06:11:13 -0500

    feat: develop restaurant login process

--- src/libs/Error.ts
+++ src/libs/Error.ts
@@ -1,30 +1,33 @@
 export enum HttpCode {
-    OK = 200,
-    CREATED = 201,
-    NOT_MODIFIED = 304,
-    BAD_REQUEST = 400,  
-    UNAUTHORIZED = 401,
-    FORBIDDEN = 403,
-    NOT_FOUND = 404,
-    INTERNAL_SERVER_ERROR = 500,
+  OK = 200,
+  CREATED = 201,
+  NOT_MODIFIED = 304,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  INTERNAL_SERVER_ERROR = 500,
 }
 
 export enum Message {
-    SOMETHING_WENT_WRONG = "SOMETHING_WENT_WRONG",
-    NO_DATA_FOUND = "NO_DATA_FOUND",
-    CREATE_FAILED = "CREATE_FAILED",
-    UPDATE_FAILED = "UPDATE_FAILED",
+  SOMETHING_WENT_WRONG = "SOMETHING_WENT_WRONG",
+  NO_DATA_FOUND = "NO_DATA_FOUND",
+  CREATE_FAILED = "CREATE_FAILED",
+  UPDATE_FAILED = "UPDATE_FAILED",
+  USED_NICK_PHONE = " YOU ARE INSERTING ALREADY USED NICK OR PHONE!",
+  NO_MEMBER_NICK = "NO MEMBER WITH THIS MEMBER NICK!",
+  WRONG_PASSWORD = "WRONG PASSWORD, PLEASE TRY AGAIN!",
 }
 
 class Errors extends Error {
-    public code: HttpCode;
-    public message: Message;
+  public code: HttpCode;
+  public message: Message;
 
-    constructor(statusCode: HttpCode, statusMessage: Message){
-        super()
-        this.code = statusCode;
-        this.message = statusMessage
-    }
+  constructor(statusCode: HttpCode, statusMessage: Message) {
+    super();
+    this.code = statusCode;
+    this.message = statusMessage;
+  }
 }
 
 export default Errors;
--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,6 +1,6 @@
 import { MemberType } from "../libs/enums/member.enum";
 import Errors, { HttpCode, Message } from "../libs/Error";
-import { MemberInput, Member } from "../libs/types/member";
+import { MemberInput, Member, LoginInput } from "../libs/types/member";
 import MemberModel from "../schema/Member.model";
 
 class MemberService {
@@ -22,6 +22,23 @@ class MemberService {
     } catch (err) {
       throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
     }
+  }
+  public async processLogin(input: LoginInput): Promise<Member> {
+    const member = await this.memberModel
+      .findOne(
+        { memberNik: input.memberNick },
+        { memberNick: 1, memberPassword: 1 }
+      )
+      .exec();
+    if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
+
+    const isMatch = input.memberPassword === member.memberPassword;
+    console.log("isMatch ", isMatch);
+    if (!isMatch) {
+      throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
+    }
+
+    return await this.memberModel.findById(member._id).exec();
   }
 }
 
