89bd5687e096a62e974bbf5f71dabeeaa08e5e02
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2024-03-11 06:09:29 -0400

    fix: create sessions on login and signup process

--- src/libs/types/member.ts
+++ src/libs/types/member.ts
@@ -1,32 +1,39 @@
+import { Request } from "express";
 import { MemberStatus, MemberType } from "../enums/member.enum";
+import { Session } from "express-session";
 
 export interface Member {
-    memberType: MemberType;
-    memberStatus: MemberStatus;
-    memberNick: string;
-    memberPhone: string;
-    memberPassword?: string;
-    memberAddress?: string;
-    memberDesc?: string;
-    memberImage?: string;
-    memberPoints: number;
-    createdAt: Date;
-    updatedAt: Date;
+  memberType: MemberType;
+  memberStatus: MemberStatus;
+  memberNick: string;
+  memberPhone: string;
+  memberPassword?: string;
+  memberAddress?: string;
+  memberDesc?: string;
+  memberImage?: string;
+  memberPoints: number;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 export interface MemberInput {
-    memberType?: MemberType;
-    memberStatus?: MemberStatus;
-    memberNick: string;
-    memberPhone: string;
-    memberPassword: string;
-    memberAddress?: string;
-    memberDesc?: string;
-    memberImage?: string;
-    memberPoints?: number;
+  memberType?: MemberType;
+  memberStatus?: MemberStatus;
+  memberNick: string;
+  memberPhone: string;
+  memberPassword: string;
+  memberAddress?: string;
+  memberDesc?: string;
+  memberImage?: string;
+  memberPoints?: number;
 }
 
 export interface LoginInput {
-    memberNick: string;
-    memberPassword: string;
+  memberNick: string;
+  memberPassword: string;
+}
+
+export interface AdminRequest extends Request {
+  member: Member;
+  session: Session & { member: Member };
 }
