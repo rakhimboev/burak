9792204a400d2276246ff1bdfd54a64a561381ce
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2024-03-22 06:30:07 -0400

    feat: front end update chosssen usser logic develop

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -1,1 +1,31 @@
 console.log("Users frontend javascript file");
+
+$(function () {
+  $(".member-status").on("change", (e) => {
+    const id = e.target.id;
+    console.log("id: " + id);
+
+    const memberStatus = $(`#${id}.member-status`).val();
+    console.log("memberStatus: " + memberStatus);
+
+    axios
+      .post("/admin/user/edit", {
+        _id: id,
+        memberStatus: memberStatus,
+      })
+      .then((response) => {
+        console.log("response: " + response);
+        const result = response.data;
+        console.log("result: " + result);
+
+        if (result.data) {
+          console.log("user updated successfully");
+          $(".member-status").blur();
+        } else alert("User update failed");
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("User update failed");
+      });
+  });
+});
